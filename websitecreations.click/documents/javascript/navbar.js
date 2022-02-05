57d20245ca2abf09432d94db5d86beadb62dc851
Website-Creations <99099311+Website-Creations@users.noreply.github.com>
2022-02-05 21:57:15 +0000

    Add files via upload

---
+++ websitecreations.click/documents/javascript/navbar.js
@@ -0,0 +1,7 @@
+function openNavbar() {
+    document.getElementById("sidenavBar").style.width = "200px";
+}
+
+function closeNavbar() {
+    document.getElementById("sidenavBar").style.width = "0";
+}
