9e24278b5035eff7f9e99aa88e836cdce3ec7c4b
Website-Creations <99099311+Website-Creations@users.noreply.github.com>
2022-02-16 23:06:16 +0000

    Delete websitecreations.click directory

--- websitecreations.click/documents/javascript/navbar.js
+++
@@ -1,7 +0,0 @@
-function openNavbar() {
-    document.getElementById("sidenavBar").style.width = "200px";
-}
-
-function closeNavbar() {
-    document.getElementById("sidenavBar").style.width = "0";
-}
